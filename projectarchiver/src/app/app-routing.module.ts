78925f2fc0516bad7447e32a5ea23f5e9b4b360f
Pasang60 <pasanggelbu@gmail.com>
2025-05-18 14:50:32 +0545

    sign up page created

--- projectarchiver/src/app/app-routing.module.ts
+++ projectarchiver/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component';
+import {SignUpComponent} from './auth/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent }
+  { path: '', component: LandingPageComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({
